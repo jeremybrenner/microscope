feb2abaac11eccdb75603f7309f2f89f7e72d1e5
jeremybrenner <jeremybrenner0@gmail.com>
2016-01-24 17:04:00 -0800

    Post editing with persmissions

---
+++ client/templates/posts/post_edit.js
@@ -0,0 +1,29 @@
+Template.postEdit.events({
+	'submit form' : function(e) {
+		e.preventDefault();
+
+		var currentPostId=this._id
+		var postProperties={
+			url: $(e.target).find().val(),
+			title: $(e.target).find().val()
+		}
+
+		Posts.update(currentPostId, {$set: postProperties}, function(error) {
+			if(error) {
+				alert(error.reason);
+			} else {
+				Router.go('postPage', {_id: currentPostId});
+			}
+		});
+	},
+
+	'click .delete' : function(e) {
+		e.preventDefault();
+
+		if(confirm("Delete this post?")) {
+			var currentPostId = this._id
+			Posts.remove(currentPostId);
+			Router.go('postsList');
+		}
+	}
+});
